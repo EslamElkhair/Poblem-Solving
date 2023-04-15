db79064c18d6617fdcb45a2f3cc16f910f0d2896
eslamelkhair <esotech3@gmail.com>
2023-04-15 19:29:12 +0200

    multiply without nul operator in another way

---
+++ multiply2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    int x,y; //x = 3 ,  y = 5
+    
+    cin>>x>>y;
+    
+    x = x+y ; // x  = 8 , 
+    
+    y = x - y; // y = 3 
+    
+    x = x - y;
+     
+    cout<<x<<" "<<y;
+    
+    return 0;
+}
